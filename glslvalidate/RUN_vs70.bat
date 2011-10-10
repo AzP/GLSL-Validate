64c2c1ff2f0c5499a896146ddb22e2a05a454e8f
Peter Asplund <peter.azp@gmail.com>
2011-10-10 20:54:01 +0200

    Remove more cruft

--- glslvalidate/RUN_vs70.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title %0
-call setenv.bat
-set VS70IDE="F:\Microsoft Visual Studio .NET\Common7\IDE\devenv.exe"
-%VS70IDE% vs7.0\glslvalidate.vcproj
--- glslvalidate/sample.vert
+++
@@ -1,8 +0,0 @@
-varying vec3 color;
-
-void main()
-{
-    color = vec3(1.0, 1.0, 1.0);
-
-    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-}
